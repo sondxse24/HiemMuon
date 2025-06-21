00bcff6d69af8c15686c867ec0d5787a1155608f
sondx24 <sondxse181792@fpt.edu.vn>
2025-06-21 08:11:05 +0700

    HiemMuon

--- src/main/java/hsf302/com/hiemmuon/dto/createDto/CreateCycleDTO.java
+++ src/main/java/hsf302/com/hiemmuon/dto/createDto/CreateCycleDTO.java
@@ -1,5 +1,6 @@
 package hsf302.com.hiemmuon.dto.createDto;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,9 +13,19 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateCycleDTO {
+
+    @Positive(message = "customerId phải lớn hơn 0")
     private int customerId;
+
+    @Positive(message = "serviceId phải lớn hơn 0")
     private int serviceId;
+
+    @NotNull(message = "startDate không được để trống")
+    @FutureOrPresent(message = "startDate phải là ngày hiện tại hoặc tương lai")
     private LocalDate startDate;
+
+    @NotNull(message = "endDate không được để trống")
     private LocalDate endDate;
+
     private String note;
 }
